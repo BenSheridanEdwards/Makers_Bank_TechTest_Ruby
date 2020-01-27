c4efa38189438c40fcb3368396fffed403ff4b60
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-27 18:24:40 +0000

    Refactor print method

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,5 +1,4 @@
 class Statement
-
   attr_reader :transaction_log
 
   def initialize
@@ -11,7 +10,7 @@ class Statement
   end
 
   def print
-    return "date || credit || debit || balance" + "\n" +
+    return "date || credit || debit || balance\n" +
     @transaction_log.flatten.reverse.map { |transaction| "#{transaction[:date]} || #{"%.2f" % transaction[:credit] if transaction[:credit]} || #{"%.2f" % transaction[:debit] if transaction[:debit]} || #{"%.2f" % transaction[:balance]}" }.join("\n")
   end
 end
