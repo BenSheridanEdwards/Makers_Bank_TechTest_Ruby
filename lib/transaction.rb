a4129d70cf353ca3cbedc9dc0f3a8ba7b2141345
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-27 16:06:16 +0000

    Add debit method to transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction
+
+  def debit(amount, date, balance)
+    return [date: date, credit: nil, debit: amount, balance: balance]
+  end
+
+  
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,4 +1,12 @@
 require 'transaction'
 
 describe Transaction do
+
+  subject(:transaction) { Transaction.new }
+
+  describe '#debit' do
+    it 'receives the amount, date, and balance, and returns them in a hash' do
+      expect(subject.debit(2000.00, "01/01/2020", 2000.00)).to eq([date: "01/01/2020", credit: nil, debit: 2000.00, balance: 2000.00])
+    end
+  end
 end
