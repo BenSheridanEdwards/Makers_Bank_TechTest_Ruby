688fbd9be90dc217361038dde34f391aa65d6061
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-27 12:29:37 +0000

    Add coverage

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -18,7 +18,16 @@ describe Account do
   describe '#withdraw' do
     it 'descreases the account balance by a given amount' do
       account.withdraw(500)
-      expect(account.balance).to eq(2500.00)
+      expect(account.balance).to eq(500.00)
+    end
+  end
+
+  context 'When a client starts their account with 1000.00, despoits 2000.00, then withdraws 500.00' do
+    it 'should return a remaining balance of 2500.00' do
+      client_account = Account.new(1000.00)
+      client_account.deposit(2000.00)
+      client_account.withdraw(500.00)
+      expect(client_account.balance).to eq(2500.00)
     end
   end
 end
