5eb899ec6f6963cdedc23f3f0f4ad9d5d9e37dae
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-27 16:10:48 +0000

    Add credit method to transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,11 @@
 class Transaction
 
   def debit(amount, date, balance)
+
     return [date: date, credit: nil, debit: amount, balance: balance]
   end
 
-  
+  def credit(amount, date, balance)
+    return [date: date, credit: amount, debit: nil, balance: balance]
+  end
 end
