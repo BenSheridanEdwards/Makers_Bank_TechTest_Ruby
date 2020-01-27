805831c7a18f366c5bad0705781f7b9ecaa0d9ab
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-27 15:54:41 +0000

    Mock transaction class and expand deposit and withdraw methods

--- lib/account.rb
+++ lib/account.rb
@@ -2,18 +2,20 @@ class Account
 
   attr_reader :balance
 
-  def initialize
+  def initialize(transaction = Transaction.new)
     @balance = 0.00
-  # @statement = Statement.new
+    @transaction = transaction
   end
 
   def deposit(amount, date)
     @balance += amount
     @date = date
+    return @transaction.debit(amount, "01/01/2020", @balance)
   end
 
   def withdraw(amount, date)
     @balance -= amount
     @date = date
+    return @transaction.credit(amount, "01/01/2020", @balance)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,24 +2,30 @@ require 'account'
 
 describe Account do
 
-  subject(:account) { Account.new }
+  let(:transaction) { double :transaction }
+  let(:statement) { double :statement }
+  subject(:account) { Account.new(transaction) }
 
   it "Initializes with a zero balance, and returns it when called" do
     expect(subject.balance).to eq(0.00)
   end
 
-  # it "Initializes with a statement object, and returns to when called" do
-  #   expect(subject.statement).to be)be_an_instance_of(Statement)
-  # end
-
   describe '#deposit' do
+    
+    before(:each) do
+      allow(transaction).to receive(:debit).with(2000.00, "01/01/2020", 2000.00).and_return([date: "01/01/2020", credit: nil, debit: 2000.00, balance: 2000.00])
+    end
+
     it 'increases the account balance by a given amount' do
       subject.deposit(2000.00, "01/01/2020")
       expect(subject.balance).to eq(2000.00)
     end
 
+    it 'returns a summary of the tranaction' do
+      expect(subject.deposit(2000.00, "01/01/2020")).to eq ([date: "01/01/2020", credit: nil, debit: 2000.00, balance: 2000.00])
+    end
+
     # it 'returns a transaction detailing the date, tranaction type, amount and balance' do
-    #   expect(account.deposit(2000, 01/01/2020)).to be_an_instance_of(Transaction)
     #   tranaction = account.deposit(2000.00, 01/01/2020)
     #   expect(transaction[:date]).to eq "01/01/2020"
     #   expect(transaction[:credit]).to eq nil
@@ -29,18 +35,35 @@ describe Account do
   end
 
   describe '#withdraw' do
+    before(:each) do
+      allow(transaction).to receive(:debit).with(1000.00, "01/01/2020", 1000.00).and_return([date: "01/01/2020", credit: nil, debit: 2000.00, balance: 2000.00])
+      allow(transaction).to receive(:credit).with(500.00, "01/01/2020", 500.00).and_return([date: "01/01/2020", credit: 500.00, debit: nil, balance: 500.00])
+    end
+
     it 'descreases the account balance by a given amount' do
       account.deposit(1000.00, "01/01/2020")
       account.withdraw(500.00, "01/01/2020")
       expect(account.balance).to eq(500.00)
     end
+
+    it 'returns a summary of the tranaction' do
+      account.deposit(1000.00, "01/01/2020")
+      expect(subject.withdraw(500.00, "01/01/2020")).to eq ([date: "01/01/2020", credit: 500.00, debit: nil, balance: 500.00])
+    end
   end
 
   context 'When a client starts their account with 1000.00, despoits 2000.00, then withdraws 500.00' do
+    before(:each) do
+      allow(transaction).to receive(:debit).with(1000.00, "01/01/2020", 1000.00).and_return([date: "01/01/2020", credit: nil, debit: 1000.00, balance: 1000.00])
+      allow(transaction).to receive(:debit).with(2000.00, "01/01/2020", 3000.00).and_return([date: "01/01/2020", credit: nil, debit: 2000.00, balance: 3000.00])
+      allow(transaction).to receive(:credit).with(500.00, "01/01/2020", 2500.00).and_return([date: "01/01/2020", credit: 500.00, debit: nil, balance: 2500.00])
+    end
+    
     it 'should return a remaining balance of 2500.00' do
+      account.deposit(1000.00, "01/01/2020")
       account.deposit(2000.00, "01/01/2020")
       account.withdraw(500.00, "01/01/2020")
-      expect(account.balance).to eq(1500.00)
+      expect(account.balance).to eq(2500.00)
     end
   end
 end
