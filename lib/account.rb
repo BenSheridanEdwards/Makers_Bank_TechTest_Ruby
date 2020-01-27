16c941d8b87c542504637431bd95fa97c1944e1b
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-27 11:56:30 +0000

    Create account balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,2 +1,8 @@
 class Account
+
+  attr_reader :balance
+
+  def initialize(balance = 500)
+    @balance = balance
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,4 +1,10 @@
 require 'account'
 
 describe Account do
+
+  subject(:account) { Account.new(500) }
+
+ it "Initializes with a given starting balance, and returns it when called" do
+   expect(account.balance).to eq(500)
+ end
 end
